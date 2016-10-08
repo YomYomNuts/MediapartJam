46307b107472b97b20fa536dfc82f8d62af38f99
odrouet92 <odrouet92@gmail.com>
2016-10-08 23:27:12 +0200

    Add SplashScreen

---
+++ Assets/Scripts/SplashScreen/PressButtonOnSplash.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PressButtonOnSplash : MonoBehaviour
+{
+    public GameObject manager;
+    public int ID;
+    public Text textField;
+    public GameObject button;
+
+    private bool isWaiting;
+    private SplashScreen splashScreen;
+    private int displayedID;
+
+    void Start()
+    {
+        isWaiting = true;
+        splashScreen = manager.GetComponent<SplashScreen>();
+        displayedID = ID + 1;
+
+        StartCoroutine(WaitingOnSplash());
+    }
+    
+    IEnumerator WaitingOnSplash()
+    {
+        while(isWaiting)
+        {
+            if(Input.GetButton("Fire1_"+ ID))
+            {
+                switch(ID)
+                {
+                    case 0:
+                        splashScreen.isPlayer0Ready = true;
+                        break;
+
+                    case 1:
+                        splashScreen.isPlayer1Ready = true;
+                        break;
+
+                    case 2:
+                        splashScreen.isPlayer2Ready = true;
+                        break;
+
+                    case 3:
+                        splashScreen.isPlayer3Ready = true;
+                        break;
+                }
+
+                button.SetActive(false);
+                textField.text = "\nJoueur " + displayedID + "\n\nPrêt!";
+
+                isWaiting = false;
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/SplashScreen/SplashScreen.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    public bool isPlayer0Ready;
+    public bool isPlayer1Ready;
+    public bool isPlayer2Ready;
+    public bool isPlayer3Ready;
+
+
+    void Start ()
+    {
+        isPlayer0Ready = false;
+        isPlayer1Ready = false;
+        isPlayer2Ready = false;
+        isPlayer3Ready = false;
+
+        StartCoroutine(WaitingForPlayerInput());
+    }
+	
+    IEnumerator WaitingForPlayerInput()
+    {
+        while(!isPlayer0Ready || !isPlayer1Ready || !isPlayer2Ready || !isPlayer3Ready)
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(3.0f);
+
+        SceneManager.LoadScene(1);
+    }
+}

65dbe20ac43d433bba8a23ddf40ad1824d86b130
Guillaume Noisette <noisette.guillaume@gmail.com>
2016-10-09 12:47:31 +0200

    Add feedback ZzZ

--- Assets/Scripts/ActionVote/TiredScript.cs
+++ Assets/Scripts/ActionVote/TiredScript.cs
@@ -5,6 +5,8 @@ public class TiredScript : ComportementScript
 {
     #region Public Attributes
     public float _DivisorSpeed = 2.0f;
+    public GameObject _ZZZ;
+    public AudioClip _AudioClip;
     #endregion
 
     #region Protected Attributes
@@ -13,6 +15,7 @@ public class TiredScript : ComportementScript
     #region Private Attributes
     private float _InitialMaxSpeed;
     private float _TiredMaxSpeed;
+    private bool _PreviousFinish;
     #endregion
 
     protected override void Start()
@@ -20,14 +23,28 @@ public class TiredScript : ComportementScript
         base.Start();
         _InitialMaxSpeed = _Character._MaxSpeed;
         _TiredMaxSpeed =_Character._MaxSpeed / _DivisorSpeed;
+        _PreviousFinish = false;
     }
 
     protected override void Update()
     {
         base.Update();
         if (IsFinish())
+        {
             _Character._MaxSpeed = _TiredMaxSpeed;
+            _ZZZ.SetActive(true);
+            if (!_PreviousFinish)
+            {
+                _Character._AudioSource.Stop();
+                _Character._AudioSource.clip = _AudioClip;
+                _Character._AudioSource.Play();
+            }
+        }
         else
+        {
             _Character._MaxSpeed = _InitialMaxSpeed;
+            _ZZZ.SetActive(false);
+        }
+        _PreviousFinish = IsFinish();
     }
 }

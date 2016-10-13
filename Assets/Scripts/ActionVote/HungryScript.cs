9a7e1e0a1edc508605b28109181c70b4e6ba4724
odrouet92 <odrouet92@gmail.com>
2016-10-14 00:27:08 +0200

    - Clignotement Rouge/Noir du background des sliders de Food quand FoodTimer < 12 - Intégration du SFX Alert joué en boucle quand FoodTimer < 12 - Intégration du SFX Starving quand un perso meurt de faim - Tweak des couleurs du slider Timer Vote

--- Assets/Scripts/ActionVote/HungryScript.cs
+++ Assets/Scripts/ActionVote/HungryScript.cs
@@ -6,18 +6,25 @@ public class HungryScript : ComportementScript
 {
     #region Public Attributes
     public Slider _Slider;
+    public AudioClip _AudioClipStarving;
+
+    public float _HungryStateThreshold = 12.0f;
     #endregion
 
     #region Protected Attributes
     #endregion
 
     #region Private Attributes
+    private LowFoodFeedback _SliderFeedback;
+    private AudioSource _AudioSource;
     #endregion
 
     protected override void Start()
     {
         base.Start();
         _Slider.maxValue = _Timer;
+        _SliderFeedback = _Slider.GetComponent<LowFoodFeedback>();
+        _AudioSource = GetComponent<AudioSource>();
     }
 
     protected override void Update()
@@ -27,8 +34,17 @@ public class HungryScript : ComportementScript
 
         if (_CurrentTimer <= 0.0f)
         {
+            _AudioSource.Stop();
+            _AudioSource.clip = _AudioClipStarving;
+            _AudioSource.Play();
+
             _Character._Animator.SetBool("Dead", true);
             _Character.Dead();
+        }
+        else if(!_SliderFeedback._IsCharacterHungry && _CurrentTimer <= _HungryStateThreshold)
+        {
+            _SliderFeedback._IsCharacterHungry = true;
+            _SliderFeedback.StartLowFoodFeedback();
         }
     }
 }
